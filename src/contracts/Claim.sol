8e8780ad8b76708812ba7c5b4096eb8636916230
Captain <70141139+Captainzik@users.noreply.github.com>
2021-04-17 15:49:34 +0100

    Update Claim.sol

--- src/contracts/Claim.sol
+++ src/contracts/Claim.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./Policy.sol";
 import "./Token.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./external/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract Claim {
